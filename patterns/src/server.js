d7961a3fe15543bbca5326e0318c8fa5fea1a0af
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-25 13:43:25 -0300

    Routers ok

--- patterns/src/server.js
+++ patterns/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const router = require("./routes");
 
 const server = express();
 server.use(morgan("dev"));
-
+//Parseo a formato json
+server.use(express.json());
+server.use(router);
 module.exports = server;
